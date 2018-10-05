8a9d4442f0c83a737ceecd9f23e6cf5257347956
Laurent Goderre <laurent.goderre@gmail.com>
2018-10-05 11:29:38 -0400

    Seperated the logic to a parent-based object

--- index.js
+++ index.js
@@ -1,6 +1,69 @@
 module.exports = (function () {
   'use strict';
 
+  var ParentBased = function(config) {
+    this.config = config;
+    this.roots = [];
+    this.temp = {};
+    this.pendingChildOf = {};
+  }
+
+  ParentBased.prototype.processItem = function(id, el) {
+    function initPush(arrayName, obj, toPush) {
+      if (obj[arrayName] === undefined) {
+        obj[arrayName] = [];
+      }
+      obj[arrayName].push(toPush);
+    }
+
+    function multiInitPush(arrayName, obj, toPushArray) {
+      var len;
+      len = toPushArray.length;
+      if (obj[arrayName] === undefined) {
+        obj[arrayName] = [];
+      }
+      while (len-- > 0) {
+        obj[arrayName].push(toPushArray.shift());
+      }
+    }
+
+    var parent = el[this.config.parent];
+    this.temp[id] = el;
+    if (parent === undefined || parent === null) {
+      // Current object has no parent, so it's a root element.
+      this.roots.push(el);
+    } else {
+      if (this.temp[parent] !== undefined) {
+        // Parent is already in temp, adding the current object to its children array.
+        initPush(this.config.children, this.temp[parent], el);
+      } else {
+        // Parent for this object is not yet in temp, adding it to pendingChildOf.
+        initPush(parent, this.pendingChildOf, el);
+      }
+      if (this.config.options.deleteParent) {
+        delete el[this.config.parent];
+      }
+    }
+    if (this.pendingChildOf[id] !== undefined) {
+      // Current object has children pending for it. Adding these to the object.
+      multiInitPush(this.config.children, el, this.pendingChildOf[id]);
+    }
+  }
+
+  ParentBased.prototype.getRoots = function() {
+    var nested;
+
+    if (this.roots.length === 1) {
+      nested = this.roots[0];
+    } else if (this.roots.length > 1) {
+      nested = {};
+      nested[this.config.children] = this.roots;
+    } else {
+      nested = {};
+    }
+    return nested;
+  }
+
   /**
    * Create a new FlatToNested object.
    *
@@ -21,66 +84,20 @@ module.exports = (function () {
    * @param {array} flat The array with the hierachy flat representation.
    */
   FlatToNested.prototype.convert = function (flat) {
-    var i, len, temp, roots, id, parent, nested, pendingChildOf, flatEl;
+    var i, len, id, flatEl;
     i = 0;
-    roots = [];
-    temp = {};
-    pendingChildOf = {};
+
+    var base = new ParentBased(this.config);
 
     for (i, len = flat.length; i < len; i++) {
       flatEl = flat[i];
       id = flatEl[this.config.id];
-      parent = flatEl[this.config.parent];
-      temp[id] = flatEl;
-      if (parent === undefined || parent === null) {
-        // Current object has no parent, so it's a root element.
-        roots.push(flatEl);
-      } else {
-        if (temp[parent] !== undefined) {
-          // Parent is already in temp, adding the current object to its children array.
-          initPush(this.config.children, temp[parent], flatEl);
-        } else {
-          // Parent for this object is not yet in temp, adding it to pendingChildOf.
-          initPush(parent, pendingChildOf, flatEl);
-        }
-        if (this.config.options.deleteParent) {
-          delete flatEl[this.config.parent];
-        }
-      }
-      if (pendingChildOf[id] !== undefined) {
-        // Current object has children pending for it. Adding these to the object.
-        multiInitPush(this.config.children, flatEl, pendingChildOf[id]);
-      }
+
+      base.processItem(id, flatEl);
     }
 
-    if (roots.length === 1) {
-      nested = roots[0];
-    } else if (roots.length > 1) {
-      nested = {};
-      nested[this.config.children] = roots;
-    } else {
-      nested = {};
-    }
-    return nested;
+    return base.getRoots();
   };
-
-  function initPush(arrayName, obj, toPush) {
-    if (obj[arrayName] === undefined) {
-      obj[arrayName] = [];
-    }
-    obj[arrayName].push(toPush);
-  }
-
-  function multiInitPush(arrayName, obj, toPushArray) {
-    var len;
-    len = toPushArray.length;
-    if (obj[arrayName] === undefined) {
-      obj[arrayName] = [];
-    }
-    while (len-- > 0) {
-      obj[arrayName].push(toPushArray.shift());
-    }
-  }
 
   return FlatToNested;
 })();
